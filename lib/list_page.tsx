73ddc783e0191d98ee3a39bc8c066b95ce35f4a7
Aiman Anizan <anizanaimann1@gmail.com>
2022-10-31 13:26:06 +0800

    Add list pages

---
+++ lib/list_page.tsx
@@ -0,0 +1,19 @@
+const pages: Array<{
+  link: string;
+  title: string;
+}> = [
+  {
+    link: '/',
+    title: 'Home',
+  },
+  {
+    link: '/about',
+    title: 'About Us',
+  },
+  {
+    link: '/products',
+    title: 'Products',
+  },
+];
+
+export default pages;
