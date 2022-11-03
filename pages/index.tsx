44577103ef90346dbd7e43d14c1789dd6d365763
Aiman Anizan <anizanaimann1@gmail.com>
2022-11-03 19:50:43 +0800

    Update index page: remove head info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
-import Head from 'next/head';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Desert Bake | Homepage</title>
-        <meta name="description" content="Desert Bake homepage" />
-        <link rel="icon" href="/favicon.jpg" />
-      </Head>
-
-      <Navbar />
+      <Navbar pageTitle="Homepage" pageDescription="Desert Bake Homepage" />
 
       <main>Main</main>
 
