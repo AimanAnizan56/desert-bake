1e9073ad16f333ec36852dd3d127ed87006266ea
Aiman Anizan <anizanaimann1@gmail.com>
2022-11-03 19:50:07 +0800

    Update navbar: add heading info

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link';
 import { Box, Text, Flex, Img, IconButton } from '@chakra-ui/react';
 import { UserIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
 import pages from '../lib/list_page';
+import Head from 'next/head';
 
-const Navbar = () => {
+const Navbar = ({ pageTitle, pageDescription }: { pageTitle: string; pageDescription: string }) => {
   const Icons: { href: string; icon: JSX.Element }[] = [
     {
       href: '/user',
@@ -17,35 +18,42 @@ const Navbar = () => {
   ];
 
   return (
-    <Box as="nav" bgColor="white" boxShadow="base" py="1rem" px="5rem" fontSize="1.2rem">
-      <Flex justify="space-between" alignItems="center">
-        <Box>
-          <Link href="/">
-            <Text as="b" fontSize="1.3rem">
-              <Img src="/img/logo.png" alt="Desert Bake" h="3rem" />
-            </Text>
-          </Link>
-        </Box>
+    <>
+      <Head>
+        <title>Desert Bake | {pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        <link rel="icon" href="/favicon.jpg" />
+      </Head>
+      <Box as="nav" bgColor="white" boxShadow="base" py="1rem" px="5rem" fontSize="1.2rem">
+        <Flex justify="space-between" alignItems="center">
+          <Box>
+            <Link href="/">
+              <Text as="b" fontSize="1.3rem">
+                <Img src="/img/logo.png" alt="Desert Bake" h="3rem" />
+              </Text>
+            </Link>
+          </Box>
 
-        <Box display="flex" gap="2rem">
-          {pages.map((page, index) => (
-            <Fragment key={index}>
-              <Link href={page.link}>
-                <Text _hover={{ color: 'brand.400' }}>{page.title}</Text>
-              </Link>
-            </Fragment>
-          ))}
-        </Box>
+          <Box display="flex" gap="2rem">
+            {pages.map((page, index) => (
+              <Fragment key={index}>
+                <Link href={page.link}>
+                  <Text _hover={{ color: 'brand.400' }}>{page.title}</Text>
+                </Link>
+              </Fragment>
+            ))}
+          </Box>
 
-        <Box display="flex" gap="1rem">
-          {Icons.map((icon, i) => (
-            <Box key={i} boxSize="1.3rem" _hover={{ color: 'brand.400' }}>
-              <Link href={icon.href}>{icon.icon}</Link>
-            </Box>
-          ))}
-        </Box>
-      </Flex>
-    </Box>
+          <Box display="flex" gap="1rem">
+            {Icons.map((icon, i) => (
+              <Box key={i} boxSize="1.3rem" _hover={{ color: 'brand.400' }}>
+                <Link href={icon.href}>{icon.icon}</Link>
+              </Box>
+            ))}
+          </Box>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
