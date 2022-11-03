e35e7f4b256a77bba26cf08e584854c1342ba0ca
Aiman Anizan <anizanaimann1@gmail.com>
2022-11-03 18:37:31 +0800

    Add custom color

--- lib/theme.tsx
+++ lib/theme.tsx
@@ -5,6 +5,20 @@ const theme = extendTheme({
     heading: `'poppins', sans-serif`,
     body: `'poppins', sans-serif`,
   },
+  colors: {
+    brand: {
+      '50': '#F7F2EE',
+      '100': '#E9D9CE',
+      '200': '#DAC1AF',
+      '300': '#CCA98F',
+      '400': '#BD9170',
+      '500': '#AF7950',
+      '600': '#8C6140',
+      '700': '#694830',
+      '800': '#463020',
+      '900': '#231810',
+    },
+  },
 });
 
 export default theme;
