e7fd493f4d068271b8dcfa43a9339bbd24760131
Aiman Anizan <anizanaimann1@gmail.com>
2022-11-26 21:03:44 +0800

    Add global style

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,6 +1,13 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        boxSizing: 'border-box',
+      },
+    },
+  },
   fonts: {
     heading: `'poppins', sans-serif`,
     body: `'poppins', sans-serif`,
