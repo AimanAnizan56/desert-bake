a6ab8e214d12fa246f5930131d25aedf0264a68d
Aiman Anizan <anizanaimann1@gmail.com>
2022-11-30 02:39:44 +0800

    Remove `next-auth` package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../lib/theme';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider resetCSS theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
