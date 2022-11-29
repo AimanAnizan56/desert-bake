623d5d116f71a8c0511579d54fc604db1aa7cb80
Aiman Anizan <anizanaimann1@gmail.com>
2022-11-30 04:21:39 +0800

    Add option for iron session

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,6 +1,16 @@
+import { IronSessionOptions } from "iron-session";
+
 export const hashPassword = (password: string) => {
   const crypto = require('crypto');
   const hash = crypto.createHash('sha256').update(password).digest();
 
   return hash.toString('hex');
 };
+
+export const ironSessionOptions = {
+  cookieName: process.env.COOKIE_NAME,
+  password: process.env.COOKIE_PASSWORD,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+} as IronSessionOptions;
