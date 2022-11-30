c06187fd77e09fefc2f7a12a1bfb9cfbcb5e2020
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-01 01:23:14 +0800

    Add method to get current customer data

--- pages/api/v1/customer/auth.ts
+++ pages/api/v1/customer/auth.ts
@@ -14,12 +14,34 @@ declare module 'iron-session' {
 }
 
 const loginRoute = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, password } = req.body;
+  if (req.method != 'POST' && req.method == 'GET') {
+    // GET-- get current customer login cookie
+    const user = req.session.user;
 
+    // if customer not login
+    if (user == undefined) {
+      res.status(400).json({ login: false, message: 'Customer not login yet' });
+      return;
+    }
+
+    res.status(200).json(user);
+    return;
+  } else if (req.method != 'POST') {
+    res.status(400).json({ error: true, message: 'Request method not allowed' });
+    return;
+  }
+
+  // POST - Authenticate user begin
+  if (req.session.user != undefined) {
+    res.status(400).json({ error: false, message: 'Customer has already login' });
+    return;
+  }
+
+  const { email, password } = req.body;
   const data = await Customer.getCustomerData(email, password);
 
   if (data.length == 0) {
-    res.status(401).json({ ok: false });
+    res.status(401).json({ error: true, message: 'Login failed' });
     return;
   }
 
@@ -30,7 +52,7 @@ const loginRoute = async (req: NextApiRequest, res: NextApiResponse) => {
   };
 
   await req.session.save();
-  res.status(200).json({ ok: true });
+  res.status(200).json({ error: false, message: 'Login success' });
 };
 
 export default withIronSessionApiRoute(loginRoute, ironSessionOptions);
