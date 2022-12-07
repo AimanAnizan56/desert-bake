9f25454ee86ccbd8669491e62017df33a5249317
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-07 19:41:47 +0800

    Change loading layout

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,11 +1,11 @@
-import { CircularProgress, Flex } from '@chakra-ui/react';
+import { Box, CircularProgress, Flex } from '@chakra-ui/react';
 
 export const Loading = () => {
   return (
-    <>
-      <Flex justifyContent={'center'} alignItems={'center'} bg="gray.50" h={'100vh'}>
+    <Box as="div" position={'fixed'} top={'0'} left={'0'} w={'100vw'} h={'100vh'} cursor={'progress'}>
+      <Flex justifyContent={'center'} alignItems={'center'} w={'100vw'} h={'100vh'} bg={'transparent'} bgColor={'rgba(255, 255, 255, 0.64)'}>
         <CircularProgress isIndeterminate size={130} color="brand.300" />
       </Flex>
-    </>
+    </Box>
   );
 };
