4116e5208a4298500eac5dd58ce2bb6ded08aa63
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-09 00:11:06 +0800

    Add carousel swiper and bounce animatation for div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import { withIronSessionSsr } from 'iron-session/next';
 import { GetServerSideProps } from 'next';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { Box, Heading, keyframes, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { ChevronDoubleDownIcon } from '@heroicons/react/24/outline';
 import Navbar from '../components/Navbar';
 import { ironSessionOptions } from '../lib/helper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export default function Home(props: any) {
   const [user, setUser] = useState<{
@@ -11,6 +20,16 @@ export default function Home(props: any) {
     email: string;
     admin: boolean;
   }>();
+
+  const bounceKayframes = keyframes`
+  0% { transform: translateY(5px) }
+  25% { transform: translateY(-5px) }
+  50% { transform: translateY(5px) }
+  75% { transform: translateY(-5px) }
+  100% { transform: translateY(5px) }
+`;
+
+  const bounceAnimation = `${bounceKayframes} 2s ease-in-out infinite`;
 
   useEffect(() => {
     if (Object.keys(props.user).length != 0) {
@@ -25,15 +44,29 @@ export default function Home(props: any) {
       {user ? <Navbar pageTitle="Homepage" pageDescription="Desert Bake Homepage" user={user} /> : <Navbar pageTitle="Homepage" pageDescription="Desert Bake Homepage" />}
 
       <main>
-        <div>Homepage</div>
-        {user && (
-          <div>
-            User is set to
-            <div>Id: {user.id}</div>
-            <div>Name: {user.name}</div>
-            <div>Email: {user.email}</div>
-          </div>
-        )}
+        <Swiper pagination={{ dynamicBullets: true }} modules={[Pagination]} style={{ background: 'gray' }}>
+          <SwiperSlide>
+            <Box as="div" position={'relative'} cursor={'default'}>
+              <Image src={'/img/carousel-1.jpg'} width={1500} height={500} alt={'caraousel image 1'} />
+
+              <Box as="span" textShadow={'1px 1px 1px rgb(0,0,0)'} color={'white'} position={'absolute'} top={'50%'} right={'50%'} transform={'translate(50%,-50%)'}>
+                <Text fontSize={'2xl'} color={'brand.500'} fontWeight={'600'}>
+                  Fresh & Delicious
+                </Text>
+
+                <Heading textAlign={'center'} as="h1">
+                  Welcome to Desert Bake
+                </Heading>
+
+                <Text textAlign={'center'}>Premium Quality & Tasty Products</Text>
+
+                <Box as={motion.div} animation={bounceAnimation} bg={'brand.500'} mx={'auto'} mt={'0.8rem'} display={'flex'} alignItems={'center'} justifyContent={'center'} gap={'0.5rem'} w={'10rem'} py={'0.5rem'} px={'0.2rem'} borderRadius={30}>
+                  Shop Now <ChevronDoubleDownIcon width="24px" height="24px" />
+                </Box>
+              </Box>
+            </Box>
+          </SwiperSlide>
+        </Swiper>
       </main>
 
       <footer>Footer</footer>
