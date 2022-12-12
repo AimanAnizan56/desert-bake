105be5a818a354793237a962f6f7033071723b52
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-13 00:03:01 +0800

    Change import path

--- pages/api/v1/admin/auth.ts
+++ pages/api/v1/admin/auth.ts
@@ -1,6 +1,6 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import AdminController from '../../../../controller/Customer.controller';
+import AdminController from '../../../../controller/Admin.controller';
 import { ironSessionOptions } from '../../../../lib/helper';
 
 const loginRoute = async (req: NextApiRequest, res: NextApiResponse) => {
