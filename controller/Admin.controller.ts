ccbd1ad0963a98ff6e57474006476ed9342d8ebb
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-13 00:04:58 +0800

    Add code for admin login

--- controller/Admin.controller.ts
+++ controller/Admin.controller.ts
@@ -1,7 +1,28 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import Admin from '../model/Admin.model';
 
-export class Admin {
+export default class AdminController {
   static login = async (req: NextApiRequest, res: NextApiResponse) => {
-    // todo - auth admin login
-  }
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({
+        message: 'Email or password field is empty',
+      });
+      return;
+    }
+
+    const data = await Admin.getAdminData(email, password);
+
+    if (data.length == 0) {
+      res.status(401).json({ error: true, message: 'Login failed' });
+      return;
+    }
+
+    // todo - change better response
+    res.status(200).json({
+      message: `hello ${email}`,
+      data: data,
+    });
+  };
 }
