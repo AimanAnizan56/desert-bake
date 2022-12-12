8ffaab48eb4027257d6758e33f38ffe29e0b96cc
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-13 00:03:24 +0800

    Add admin model

---
+++ model/Admin.model.ts
@@ -0,0 +1,25 @@
+import { hashPassword } from '../lib/helper';
+import { makeQuery } from '../lib/mysql_config';
+
+export default class Admin {
+  private readonly id!: number;
+  private name: string;
+  private email: string;
+  private password!: string;
+
+  constructor(name: string, email: string, password: string) {
+    this.name = name;
+    this.email = email;
+    this.password = hashPassword(password);
+  }
+
+  static getAdminData = async (email: string, password: string) => {
+    const passwordHashed = hashPassword(password);
+    const data = await makeQuery('SELECT admin_id, admin_name, admin_email FROM admin WHERE admin_email=? AND password=?', [email, passwordHashed]);
+
+    if (data.length == 0) {
+      return [];
+    }
+    return data[0];
+  };
+}
