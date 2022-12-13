84d5b1d9375447133e26cf6f1c18a7235c3cd116
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-13 18:39:11 +0800

    Add css file to override css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../lib/theme';
+import '../styles/default.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
