17e1b780e15dd766326019f56db12bf09d15b15f
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-13 12:54:17 +0800

    Add navbar for admin

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,11 +10,109 @@ type UserType = {
   id?: number;
   name?: string;
   email?: string;
+  admin?: boolean;
 };
 
 const Navbar = ({ pageTitle, pageDescription, user }: { pageTitle: string; pageDescription: string; user?: UserType }) => {
   const router = useRouter();
 
+  const title = `Desert Bake | ${pageTitle}`;
+
+  // *** for admin
+  if (user && user.admin) {
+    const Icons: { href: string; icon: JSX.Element; fc: Function }[] = [
+      {
+        href: '/admin/profile',
+        icon: <UserIcon />,
+        fc: () => {},
+      },
+      {
+        href: '/admin',
+        icon: <ArrowLeftOnRectangleIcon />,
+        fc: async (e: any) => {
+          // logout
+          e.preventDefault();
+
+          try {
+            let res = await axios.get('/api/v1/admin/logout');
+            const { logout, message } = await res.data;
+
+            if (logout) {
+              router.push('/admin/signin');
+            }
+          } catch (err) {
+            console.log('err', err);
+          }
+        },
+      },
+    ];
+
+    const pages: Array<{
+      link: string;
+      title: string;
+    }> = [
+      {
+        link: '/admin',
+        title: 'Home',
+      },
+      {
+        link: '/admin/view-products',
+        title: 'Products',
+      },
+      {
+        link: '/admin/view-orders',
+        title: 'Orders',
+      },
+    ];
+
+    return (
+      <>
+        <Head>
+          <title>{title}</title>
+          <meta name="description" content={pageDescription} />
+          <link rel="icon" href="/favicon.jpg" />
+        </Head>
+        <Box as="nav" bgColor="white" boxShadow="base" py="0.7rem" px="4rem" fontSize="1.1rem">
+          <Grid templateColumns={'1fr 1fr 1fr'} alignItems={'center'}>
+            <Box justifySelf={'start'}>
+              <Link href="/">
+                <Text as="b" fontSize="1.3rem">
+                  <Img src="/img/logo.png" alt="Desert Bake" h="3rem" />
+                </Text>
+              </Link>
+            </Box>
+
+            <Box display="flex" gap="2rem" justifySelf={'center'}>
+              {pages.map((page, index) => (
+                <Fragment key={index}>
+                  <Link href={page.link}>
+                    <Text _hover={{ color: 'brand.400' }}>{page.title}</Text>
+                  </Link>
+                </Fragment>
+              ))}
+            </Box>
+
+            <Box display="flex" gap="1rem" alignItems={'center'} justifySelf={'end'}>
+              {Icons.map((icon, i) => (
+                <Box key={i} boxSize="1.3rem" _hover={{ color: 'brand.400' }}>
+                  <Link
+                    href={icon.href}
+                    onClick={(e: Event) => {
+                      icon.fc(e);
+                    }}
+                  >
+                    {icon.icon}
+                  </Link>
+                </Box>
+              ))}
+            </Box>
+          </Grid>
+        </Box>
+      </>
+    );
+  }
+
+  // *** For customer
   const Icons: { href: string; icon: JSX.Element; fc: Function }[] = [
     {
       href: '/customer',
@@ -73,8 +171,6 @@ const Navbar = ({ pageTitle, pageDescription, user }: { pageTitle: string; pageD
       title: 'About',
     },
   ];
-
-  const title = `Desert Bake | ${pageTitle}`;
 
   return (
     <>
