f89d6aaef6ab894dddcb2883b1ccf8a24dbc9410
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-13 14:19:41 +0800

    Add products page for admin

---
+++ pages/admin/products/index.tsx
@@ -0,0 +1,36 @@
+import { withIronSessionSsr } from 'iron-session/next';
+import { GetServerSideProps } from 'next';
+import Navbar from '../../../components/Navbar';
+import { ironSessionOptions } from '../../../lib/helper';
+
+const Products = (props: any) => {
+  return (
+    <>
+      <Navbar pageTitle="Products" pageDescription="Product page" user={props.user} />
+
+      <main>This is products for admin page</main>
+    </>
+  );
+};
+
+export default Products;
+
+export const getServerSideProps: GetServerSideProps = withIronSessionSsr(async ({ req }) => {
+  const user = req.session.user;
+
+  if (user && user.admin) {
+    return {
+      props: {
+        user: user,
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      destination: '/admin/signin',
+      permanent: false,
+    },
+    props: {},
+  };
+}, ironSessionOptions);
