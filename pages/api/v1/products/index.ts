4c520b4cfbb8f6d73d41d24c488298d8d9cee912
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-15 21:13:47 +0800

    Add code to parse request body

--- pages/api/v1/products/index.ts
+++ pages/api/v1/products/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import multer from 'multer';
+import multiparty from 'multiparty';
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -8,6 +9,23 @@ const upload = multer({
   }),
 });
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  console.log('content-type', req.headers['content-type']);
+  const form = new multiparty.Form();
+  const data = await new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject({ err });
+      resolve({ fields, files });
+    });
+  });
+
+  console.log('Form data: ', data);
+
+  return res.status(200).json({ data });
 }
