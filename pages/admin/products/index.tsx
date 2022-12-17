e3d5f948620eac13c6ae04fbd45dc2edb96ec6a9
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-17 21:18:42 +0800

    Retrieve product from API

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,11 +1,31 @@
-import { Box, Button, Container, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Grid, GridItem, Heading, Skeleton, SkeletonText } from '@chakra-ui/react';
 import { withIronSessionSsr } from 'iron-session/next';
 import { GetServerSideProps } from 'next';
 import Link from 'next/link';
+import { Fragment, useEffect, useState } from 'react';
 import Navbar from '../../../components/Navbar';
 import { ironSessionOptions } from '../../../lib/helper';
+import { SkeletonProductGridItem } from '../../../components/Skeleton.component';
+import axios from 'axios';
+import Image from 'next/image';
+import { EditIcon } from '@chakra-ui/icons';
 
 const Products = (props: any) => {
+  const [products, setProducts] = useState<Array<any>>();
+
+  useEffect(() => {
+    const callAPI = async () => {
+      const res = await axios.get('/api/v1/products');
+      const { data } = res.data;
+
+      if (data) {
+        setProducts(data);
+      }
+    };
+
+    callAPI();
+  }, []);
+
   return (
     <>
       <Navbar pageTitle="Products" pageDescription="Product page" user={props.user} currentPage={'Products'} />
@@ -23,6 +43,42 @@ const Products = (props: any) => {
               </Button>
             </Box>
           </Flex>
+
+          <Grid templateColumns={'repeat(3, 1fr)'} gap={5} my={'2.5rem'}>
+            {!products && (
+              <>
+                <SkeletonProductGridItem />
+                <SkeletonProductGridItem />
+                <SkeletonProductGridItem />
+              </>
+            )}
+
+            {products &&
+              products.map((product, i) => {
+                return (
+                  <Fragment key={i}>
+                    <GridItem boxShadow={'var(--box-shadow)'} borderRadius={'5px'} px={'0.5rem'} py={'1rem'}>
+                      <Box as="div" mb={'0.5rem'} position={'relative'} width={'100%'} height={'150px'}>
+                        <Image fill src={product.product_image_path} alt={product.product_id} />
+                      </Box>
+                      <Box as="div" fontWeight={'bold'}>
+                        {product.product_name}
+                      </Box>
+                      <Box as="div" mb={'0.5rem'} fontWeight={'bold'} color={'gray.400'}>
+                        RM {product.product_price}
+                      </Box>
+                      <Box as="div">
+                        <Link href={`/admin/products/${product.product_id}`}>
+                          <Button leftIcon={<EditIcon />} colorScheme={'brand'} width={'100%'}>
+                            Edit
+                          </Button>
+                        </Link>
+                      </Box>
+                    </GridItem>
+                  </Fragment>
+                );
+              })}
+          </Grid>
         </Container>
       </main>
     </>
