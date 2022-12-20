5b3917de55f3d33682006c367ae2c1a5ee7985dc
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-20 17:17:41 +0800

    Add heading and filter button

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,3 +1,5 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Box, Button, Container, Flex, Heading, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { withIronSessionSsr } from 'iron-session/next';
 import { GetServerSideProps } from 'next';
 import { useState, useEffect } from 'react';
@@ -12,6 +14,12 @@ const Products = (props: any) => {
     admin: boolean;
   }>();
 
+  const [filter, setFilter] = useState<'All Products' | 'Dessert' | 'Beverage' | 'Bread'>('All Products');
+
+  const filterHandler = (e: any) => {
+    setFilter(e.target.dataset.value);
+  };
+
   useEffect(() => {
     if (Object.keys(props.user).length != 0) {
       setUser({
@@ -23,7 +31,40 @@ const Products = (props: any) => {
   return (
     <>
       {user ? <Navbar pageTitle="List of Products" pageDescription="This is page that display all available products" currentPage={'Products'} user={user} /> : <Navbar pageTitle="List of Products" pageDescription="This is page that display all available products" currentPage="Products" />}
-      <main>This is product</main>
+      <main>
+        <Container maxW={'container.lg'}>
+          <Box as="div" mt={'2rem'}>
+            <Flex alignItems={'center'} justifyContent="space-between">
+              <Heading as="h1" fontSize={'1.5rem'}>
+                {filter}
+              </Heading>
+
+              <Box as="div">
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<ChevronDownIcon />} colorScheme={'brand'} variant="outline">
+                    Filter
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={filterHandler} data-value={'All Product'}>
+                      All
+                    </MenuItem>
+                    <MenuItem onClick={filterHandler} data-value={'Dessert'}>
+                      Dessert
+                    </MenuItem>
+                    <MenuItem onClick={filterHandler} data-value={'Beverage'}>
+                      Beverage
+                    </MenuItem>
+                    <MenuItem onClick={filterHandler} data-value={'Bread'}>
+                      Bread
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Box>
+            </Flex>
+          </Box>
+        </Container>
+      </main>
+
       <footer>footer </footer>
     </>
   );
