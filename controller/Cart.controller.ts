b4b5ccab50f1f90ef1a76b4890f2524e42499af1
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-27 23:37:39 +0800

    Check user login as customer/admin

--- controller/Cart.controller.ts
+++ controller/Cart.controller.ts
@@ -3,6 +3,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default class CartItemController {
   static addToCart = async (req: NextApiRequest, res: NextApiResponse) => {
     // todo - add to cart
+    if (!req.session.user) {
+      res.status(400).json({
+        message: 'Please login first',
+      });
+      return;
+    }
+
+    if (req.session.user && req.session.user.admin) {
+      res.status(400).json({
+        message: 'Only customer can add to cart',
+      });
+      return;
+    }
+
     // check if cart already exist by status (cart model)
     // get cart is if cart exist (cart model)
     // if not exist, create new cart with params customer id (cart model)
