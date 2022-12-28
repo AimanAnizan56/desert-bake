92f95f51b123910391b81e617d8b9aafa0cd2fe9
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-28 18:45:09 +0800

    Fix variable name

--- model/Items.model.ts
+++ model/Items.model.ts
@@ -8,23 +8,29 @@ export default class Item {
   private item_quantity!: number;
 
   setItem = async (product_id: number, cart_id: number) => {
-    this.product_id = product_id;
-    this.cart_id = cart_id;
-    let row: any = await makeQuery('SELECT item_id, item_quantity FROM items WHERE product_id=? AND cart_id=?', [this.product_id, this.cart_id]);
-    const temp = row;
+    try {
+      this.product_id = product_id;
+      this.cart_id = cart_id;
+      let row: any = await makeQuery('SELECT item_id, item_quantity FROM items WHERE product_id=? AND cart_id=?', [this.product_id, this.cart_id]);
+      const temp = row;
 
-    row = await makeQuery('SELECT product_price as item_price FROM product WHERE product_id=?', [this.product_id]);
-    this.item_price = row[0].item_price;
+      row = await makeQuery('SELECT product_price as item_price FROM product WHERE product_id=?', [this.product_id]);
+      this.item_price = row[0].item_price;
 
-    if (temp.length > 0) {
-      this.item_id = row[0].item_id;
-      this.item_quantity = row[0].item_quantity;
-      return;
+      if (temp.length > 0) {
+        this.item_id = temp[0].item_id;
+        this.item_quantity = temp[0].item_quantity;
+        return;
+      }
+
+      this.item_quantity = 0;
+      row = await makeQuery('INSERT INTO items(product_id, cart_id, item_price, item_quantity) VALUES (?,?,?,?)', [this.product_id, this.cart_id, this.item_price, this.item_quantity]);
+      this.item_id = row.insertId;
+    } catch (err) {
+      console.log('====================================');
+      console.log('setItem Err: ', err);
+      console.log('====================================');
     }
-
-    this.item_quantity = 0;
-    row = await makeQuery('INSERT INTO items(product_id, cart_id, item_price, item_quantity) VALUES (?,?,?,?)', [this.product_id, this.cart_id, this.item_price, this.item_quantity]);
-    this.item_id = row.insertId;
   };
 
   addToCart = async () => {
