9f37cb206c1405d9acfaa2b464fc1b65f9c82d1b
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-28 20:29:32 +0800

    Add `removeFromCart` method

--- controller/Cart.controller.ts
+++ controller/Cart.controller.ts
@@ -5,7 +5,6 @@ import Product from '../model/Product.model';
 
 export default class CartItemController {
   static addToCart = async (req: NextApiRequest, res: NextApiResponse) => {
-    // todo - add to cart
     if (!req.session.user) {
       res.status(400).json({
         message: 'Please login first',
@@ -20,7 +19,6 @@ export default class CartItemController {
       return;
     }
 
-    const customerId = req.session.user.id;
     // check if cart already exist by status (cart model)
     // get cart id if cart exist (cart model)
     // if not exist, create new cart with params customer id (cart model)
@@ -69,6 +67,68 @@ export default class CartItemController {
 
     res.status(200).json({
       message: 'Successfully add to cart',
+    });
+  };
+
+  static removeFromCart = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!req.session.user) {
+      res.status(400).json({
+        message: 'Please login first',
+      });
+      return;
+    }
+
+    if (req.session.user && req.session.user.admin) {
+      res.status(400).json({
+        message: 'Only customer can remove to cart',
+      });
+      return;
+    }
+
+    const userCart = new Cart();
+    userCart.setCustomerId(parseInt(req.session.user.id));
+
+    const { cartId }: any = await userCart.getUserCartId();
+
+    if (cartId == -99) {
+      res.status(500).json({
+        message: 'Internal server error',
+      });
+      return;
+    }
+
+    // use cart id to add into item - use method in item model (item model)
+    const { product_id } = req.body;
+
+    if (product_id == undefined) {
+      res.status(400).json({
+        message: 'Please include product id',
+      });
+    }
+
+    // check product id exist or not
+    const product = await Product.getProduct(product_id);
+
+    if (product.length == 0) {
+      res.status(400).json({
+        message: 'Product with such id not exist',
+      });
+      return;
+    }
+
+    const item = new Item();
+    await item.setItem(product_id, cartId);
+
+    const success = await item.removeFromCart();
+
+    if (!success) {
+      res.status(500).json({
+        message: 'Internal server error. Cannot add to cart',
+      });
+    }
+
+    res.status(200).json({
+      message: 'Successfully remove to cart',
     });
   };
 
