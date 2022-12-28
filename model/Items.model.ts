e45c725d8c9f398ccd594ac0ea3779f9484c38e2
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-28 18:32:21 +0800

    Add setter method

--- model/Items.model.ts
+++ model/Items.model.ts
@@ -1,3 +1,5 @@
+import { makeQuery } from '../lib/mysql_config';
+
 export default class Item {
   private item_id!: number;
   private product_id!: number;
@@ -5,11 +7,33 @@ export default class Item {
   private item_price!: number;
   private item_quantity!: number;
 
-  setItem = (item_id: number, product_id: number, cart_id: number, item_price: number, item_quantity: number) => {
-    this.item_id = item_id;
+  setItem = async (product_id: number, cart_id: number) => {
     this.product_id = product_id;
     this.cart_id = cart_id;
-    this.item_price = item_price;
-    this.item_quantity = item_quantity;
+    let row: any = await makeQuery('SELECT item_id, item_quantity FROM items WHERE product_id=? AND cart_id=?', [this.product_id, this.cart_id]);
+    const temp = row;
+
+    row = await makeQuery('SELECT product_price as item_price FROM product WHERE product_id=?', [this.product_id]);
+    this.item_price = row[0].item_price;
+
+    if (temp.length > 0) {
+      this.item_id = row[0].item_id;
+      this.item_quantity = row[0].item_quantity;
+      return;
+    }
+
+    this.item_quantity = 0;
+    row = await makeQuery('INSERT INTO items(product_id, cart_id, item_price, item_quantity) VALUES (?,?,?,?)', [this.product_id, this.cart_id, this.item_price, this.item_quantity]);
+    this.item_id = row.insertId;
+  };
+
+  output = () => {
+    console.log('====================================');
+    console.log('item_id: ', this.item_id);
+    console.log('product_id: ', this.product_id);
+    console.log('cart_id: ', this.cart_id);
+    console.log('item_price: ', this.item_price);
+    console.log('item_quantity: ', this.item_quantity);
+    console.log('====================================');
   };
 }
