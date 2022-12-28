cd9845c8aa5542feba9334f3978f2afb2932fced
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-28 20:41:42 +0800

    Add api route to the cartitem controller

---
+++ pages/api/v1/cart/remove.ts
@@ -0,0 +1,17 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { ironSessionOptions } from '../../../../lib/helper';
+import nextConnect from 'next-connect';
+import { NextApiRequest, NextApiResponse } from 'next';
+import CartItemController from '../../../../controller/Cart.controller';
+
+const cartHandler = nextConnect({
+  onNoMatch: (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(405).json({ error: true, message: 'Request method not allowed' });
+  },
+});
+
+cartHandler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  await CartItemController.removeFromCart(req, res);
+});
+
+export default withIronSessionApiRoute(cartHandler, ironSessionOptions);
