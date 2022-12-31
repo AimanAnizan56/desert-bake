99efca30bbeebdf7162ede0445989c04ed7d30dc
Aiman Anizan <anizanaimann1@gmail.com>
2022-12-31 18:53:55 +0800

    Seperate delete item to new route: use request query to get item id

--- pages/api/v1/cart/remove/[item_id].ts
+++ pages/api/v1/cart/remove/[item_id].ts
@@ -1,8 +1,8 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
-import { ironSessionOptions } from '../../../../lib/helper';
+import { ironSessionOptions } from '../../../../../lib/helper';
 import nextConnect from 'next-connect';
 import { NextApiRequest, NextApiResponse } from 'next';
-import CartItemController from '../../../../controller/Cart.controller';
+import CartItemController from '../../../../../controller/Cart.controller';
 
 const cartHandler = nextConnect({
   onNoMatch: (req: NextApiRequest, res: NextApiResponse) => {
@@ -10,8 +10,8 @@ const cartHandler = nextConnect({
   },
 });
 
-cartHandler.post(async (req: NextApiRequest, res: NextApiResponse) => {
-  await CartItemController.removeFromCart(req, res);
+cartHandler.delete(async (req: NextApiRequest, res: NextApiResponse) => {
+  await CartItemController.removeItem(req, res);
 });
 
 export default withIronSessionApiRoute(cartHandler, ironSessionOptions);
