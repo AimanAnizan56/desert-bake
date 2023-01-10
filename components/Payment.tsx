fcaed35f1543074c613284691bc59ae646a83d19
Aiman Anizan <anizanaimann1@gmail.com>
2023-01-10 17:20:44 +0800

    Add better response catch

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -2,12 +2,17 @@ import { Elements, useStripe, useElements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { PaymentElement } from '@stripe/react-stripe-js';
 import { Box, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const stripePromise = loadStripe('pk_test_51MLMB4Iojr912iY5E3Zoy28Lr8HCl9wPlHoBOfa1Qyf1AbQqIv0xc2QFgHrVk4hEpOUBnrCBJnyNC6bemvd7ZAYn005BlMRmLU');
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+
+  const [buttonState, setButtonState] = useState({
+    isLoading: false,
+  });
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -17,23 +22,57 @@ const CheckoutForm = () => {
       return;
     }
 
-    const stripeRes = await stripe?.confirmPayment({
-      elements,
-      confirmParams: {
-        // todo -- find the way to use localhost
-        return_url: `https://google.com`,
-      },
-    });
+    try {
+      setButtonState({
+        ...buttonState,
+        isLoading: true,
+      });
 
-    console.log('====================================');
-    console.log('stripe res', stripeRes);
-    console.log('====================================');
+      const stripeRes = await stripe?.confirmPayment({
+        elements,
+        confirmParams: {
+          // todo -- improvise later / ignore if there if success page
+          return_url: `${process.env.NEXT_PUBLIC_HOST_TEST}/`,
+        },
+        redirect: 'if_required',
+      });
+
+      if (stripeRes?.error) {
+        // todo - add modal to display error
+        console.log('inform that there was an error');
+        console.log(stripeRes?.error);
+        setButtonState({
+          ...buttonState,
+          isLoading: false,
+        });
+        return;
+      }
+
+      console.log('====================================');
+      console.log('Response stripe');
+      console.log(stripeRes);
+      console.log('====================================');
+
+      if (stripeRes?.paymentIntent.status == 'succeeded') {
+        // todo - redirect to success page
+        console.log('succeed');
+        setButtonState({
+          ...buttonState,
+          isLoading: false,
+        });
+      }
+    } catch (err) {
+      console.log('====================================');
+      console.log('stripe confirm payment error');
+      console.log(err);
+      console.log('====================================');
+    }
   };
 
   return (
     <form>
       <PaymentElement />
-      <Button onClick={handleSubmit} colorScheme={'brand'} mt={'1rem'} w={'100%'} disabled={!stripe}>
+      <Button onClick={handleSubmit} colorScheme={'brand'} mt={'1rem'} w={'100%'} disabled={!stripe} isLoading={buttonState.isLoading}>
         Pay Now
       </Button>
     </form>
