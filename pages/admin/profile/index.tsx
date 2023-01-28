eee6f80fa9bc1da8c357a2d062dc96983b646862
Aiman Anizan <anizanaimann1@gmail.com>
2023-01-29 01:33:49 +0800

    Add update profile form

--- pages/admin/profile/index.tsx
+++ pages/admin/profile/index.tsx
@@ -1,14 +1,309 @@
+import { Box, Container, Input, InputGroup, InputLeftElement, Heading, Button, Text, Flex, InputRightElement } from '@chakra-ui/react';
+import { EyeIcon, EyeSlashIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import { withIronSessionSsr } from 'iron-session/next';
 import { GetServerSideProps } from 'next';
 import Navbar from '../../../components/Navbar';
 import { ironSessionOptions } from '../../../lib/helper';
+import { useEffect, useState } from 'react';
+import { EmailIcon, LockIcon } from '@chakra-ui/icons';
 
 const Profile = (props: any) => {
+  type User = {
+    id: string;
+    name: string;
+    email: string;
+    admin: boolean;
+  };
+  const { user }: { user: User } = props;
+
+  const [formVal, setFormVal] = useState({
+    name: user.name,
+    email: user.email,
+  });
+
+  const [formPassVal, setFormPassVal] = useState({
+    currPass: '',
+    password: '',
+    confPassword: '',
+  });
+
+  const [currentForm, setCurrentForm] = useState('change-details');
+
+  const [buttonState, setButtonState] = useState({
+    isLoading: false,
+    isDisabled: false,
+  });
+
+  const [buttonStatePassword, setButtonStatePassword] = useState({
+    isLoading: false,
+    isDisabled: false,
+  });
+
+  const [error, setError] = useState({
+    name: false,
+    email: false,
+    currPass: false,
+    password: false,
+    confPassword: false,
+  });
+
+  const [showInput, setShowInput] = useState({
+    currPass: false,
+    password: false,
+    confPassword: false,
+  });
+
+  useEffect(() => {
+    if (formVal.name.length != 0 && formVal.email.length != 0 && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formVal.email)) {
+      setButtonState({
+        ...buttonState,
+        isDisabled: false,
+      });
+      return;
+    }
+    setButtonState({
+      ...buttonState,
+      isDisabled: true,
+    });
+  }, [formVal.name, formVal.email]);
+
+  const handleEmailValidation = () => {
+    if (formVal.email.length != 0 && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formVal.email)) {
+      setError({
+        ...error,
+        email: false,
+      });
+      return;
+    }
+    setError({
+      ...error,
+      email: true,
+    });
+
+    console.log(error);
+  };
+
+  const handlePasswordBlur = () => {
+    // do password validation
+    console.log('password validation');
+  };
+
+  const handleConfPasswordBlur = () => {
+    // do confirmpassword validation
+    console.log('confirm password validation');
+  };
+
+  const handleUpdate = async (event: any) => {
+    event.preventDefault();
+
+    const url = `/api/v1/admin/${user.id}`;
+  };
+
+  const handlePasswordUpdate = async (event: any) => {
+    event.preventDefault();
+
+    const url = `/api/v1/admin/${user.id}`;
+  };
+
   return (
     <>
       <Navbar pageTitle="My Profile" pageDescription="Profile for admin" currentPage="My Profile" user={props.user} />
 
-      <main>This is profile page for admin</main>
+      <main>
+        <Container maxW={'container.sm'} my={'2rem'}>
+          <Box as="div" mt={'2rem'} p={'2rem 1.4rem'}>
+            <Heading as="h3" size={'md'} mb={'1.5rem'}>
+              My Profile
+            </Heading>
+
+            {currentForm == 'change-details' && (
+              <form>
+                <Box as="div" mb={'0.5rem'}>
+                  <Text>Name</Text>
+
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                      <UserCircleIcon width={'20px'} height={'20px'} color={'var(--chakra-colors-gray-300)'} />
+                    </InputLeftElement>
+                    <Input type="text" placeholder="Admin Name" value={formVal.name} onChange={(e) => setFormVal({ ...formVal, name: e.target.value })} _focusVisible={{ borderColor: 'brand.400', boxShadow: '0 0 0 1px var(--chakra-colors-brand-400)' }} />
+                  </InputGroup>
+                </Box>
+
+                <Box as="div" mb={'2rem'}>
+                  <Text>Email</Text>
+
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                      <EmailIcon color={!error.email ? 'gray.300' : 'red'} />
+                    </InputLeftElement>
+                    <Input
+                      type="text"
+                      isInvalid={error.email}
+                      placeholder="hello@admin.com"
+                      value={formVal.email}
+                      onChange={(e) => setFormVal({ ...formVal, email: e.target.value })}
+                      onBlur={handleEmailValidation}
+                      _focusVisible={{ borderColor: 'brand.400', boxShadow: '0 0 0 1px var(--chakra-colors-brand-400)' }}
+                    />
+                  </InputGroup>
+                  {error.email && (
+                    <Text ml="0.3rem" color="red" fontWeight="bold" fontSize="0.7rem" mt={'0.1rem'}>
+                      Email is invalid
+                    </Text>
+                  )}
+                </Box>
+
+                <Flex gap={'0.5rem'} direction={'column'} maxW={'15rem'} mx={'auto'}>
+                  <Button colorScheme={'brand'} isLoading={buttonState.isLoading} isDisabled={buttonState.isDisabled} onClick={handleUpdate} _focusVisible={{ boxShadow: '0 0 0 3px var(--chakra-colors-brand-200)' }}>
+                    Update Profile
+                  </Button>
+                  <Button variant={'outline'} colorScheme={'brand'} onClick={() => setCurrentForm('change-password')}>
+                    Change Password
+                  </Button>
+                </Flex>
+              </form>
+            )}
+
+            {currentForm == 'change-password' && (
+              <form>
+                <Box as="div" mb="0.5rem">
+                  <Text>Current Password</Text>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                      <LockIcon color={'gray.300'} />
+                    </InputLeftElement>
+                    <Input
+                      pr="4.5rem"
+                      focusBorderColor={'brand.500'}
+                      value={formPassVal.currPass}
+                      onFocus={() => setError({ ...error, currPass: false })}
+                      onChange={(e) => setFormPassVal({ ...formPassVal, currPass: e.target.value })}
+                      type={showInput.currPass ? 'text' : 'password'}
+                      placeholder={'Enter password'}
+                      isRequired
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Box
+                        tabIndex={0}
+                        h="1.75rem"
+                        w="1rem"
+                        color={'gray.500'}
+                        cursor={'pointer'}
+                        _hover={{ color: 'gray.700' }}
+                        display="flex"
+                        justifyContent={'center'}
+                        onClick={() => setShowInput({ ...showInput, currPass: !showInput.currPass })}
+                        onKeyDown={(e) => {
+                          if (e.key == 'Enter' || e.keyCode == 32) setShowInput({ ...showInput, currPass: !showInput.currPass });
+                        }}
+                      >
+                        {showInput.currPass ? <EyeIcon /> : <EyeSlashIcon />}
+                      </Box>
+                    </InputRightElement>
+                  </InputGroup>
+                </Box>
+
+                <Box as="div" mb="0.5rem">
+                  <Text>New Password</Text>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                      <LockIcon color={'gray.300'} />
+                    </InputLeftElement>
+                    <Input
+                      pr="4.5rem"
+                      focusBorderColor={'brand.500'}
+                      value={formPassVal.password}
+                      onBlur={handlePasswordBlur}
+                      onFocus={() => setError({ ...error, password: false })}
+                      onChange={(e) => setFormPassVal({ ...formPassVal, password: e.target.value })}
+                      type={showInput.password ? 'text' : 'password'}
+                      placeholder={'Enter password'}
+                      isRequired
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Box
+                        tabIndex={0}
+                        h="1.75rem"
+                        w="1rem"
+                        color={'gray.500'}
+                        cursor={'pointer'}
+                        _hover={{ color: 'gray.700' }}
+                        display="flex"
+                        justifyContent={'center'}
+                        onClick={() => setShowInput({ ...showInput, password: !showInput.password })}
+                        onKeyDown={(e) => {
+                          if (e.key == 'Enter' || e.keyCode == 32) setShowInput({ ...showInput, password: !showInput.password });
+                        }}
+                      >
+                        {showInput.password ? <EyeIcon /> : <EyeSlashIcon />}
+                      </Box>
+                    </InputRightElement>
+                  </InputGroup>
+
+                  {error.password && (
+                    <Text ml="0.3rem" color="red" fontWeight="bold" fontSize="0.7rem" mt={'0.1rem'}>
+                      Password must be at least 8 length, including lowercase, uppercase, number and special character!
+                    </Text>
+                  )}
+                </Box>
+
+                <Box as="div" mb="2rem">
+                  <Text>Confirm Password</Text>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                      <LockIcon color={'gray.300'} />
+                    </InputLeftElement>
+                    <Input
+                      pr="4.5rem"
+                      focusBorderColor={'brand.500'}
+                      value={formPassVal.confPassword}
+                      onBlur={handleConfPasswordBlur}
+                      onFocus={() => setError({ ...error, confPassword: false })}
+                      onChange={(e) => setFormPassVal({ ...formPassVal, confPassword: e.target.value })}
+                      type={showInput.confPassword ? 'text' : 'password'}
+                      placeholder={'Enter password'}
+                      isRequired
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Box
+                        tabIndex={0}
+                        h="1.75rem"
+                        w="1rem"
+                        color={'gray.500'}
+                        cursor={'pointer'}
+                        _hover={{ color: 'gray.700' }}
+                        display="flex"
+                        justifyContent={'center'}
+                        onClick={() => setShowInput({ ...showInput, confPassword: !showInput.confPassword })}
+                        onKeyDown={(e) => {
+                          if (e.key == 'Enter' || e.keyCode == 32) setShowInput({ ...showInput, confPassword: !showInput.confPassword });
+                        }}
+                      >
+                        {showInput.confPassword ? <EyeIcon /> : <EyeSlashIcon />}
+                      </Box>
+                    </InputRightElement>
+                  </InputGroup>
+
+                  {error.confPassword && (
+                    <Text ml="0.3rem" color="red" fontWeight="bold" fontSize="0.7rem" mt={'0.1rem'}>
+                      Password did not match!
+                    </Text>
+                  )}
+                </Box>
+
+                <Flex gap={'0.5rem'} direction={'column'} maxW={'15rem'} mx={'auto'}>
+                  <Button colorScheme={'brand'} isLoading={buttonStatePassword.isLoading} isDisabled={buttonStatePassword.isDisabled} onClick={handlePasswordUpdate} _focusVisible={{ boxShadow: '0 0 0 3px var(--chakra-colors-brand-200)' }}>
+                    Update Password
+                  </Button>
+                  <Button variant={'outline'} colorScheme={'brand'} onClick={() => setCurrentForm('change-details')}>
+                    Back
+                  </Button>
+                </Flex>
+              </form>
+            )}
+          </Box>
+        </Container>
+      </main>
     </>
   );
 };
